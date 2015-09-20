0a2874cb95ec7d27d2f1703573d0893b3ccd5787
mshailesh <shailesh.meshram@gmail.com>
2015-09-20 09:22:47 -0700

    jbpm spring

---
+++ jbpm-spring/src/main/java/org/spring/boot/jbpm/Application.java
@@ -0,0 +1,31 @@
+package org.spring.boot.jbpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan 
+public class Application {
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+	}
+	
+	
+	
+
+}
