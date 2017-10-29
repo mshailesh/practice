ab4525280edc21a0a9b7cc56a5197756c3cbf817
mshailesh <shailesh.meshram@gmail.com>
2017-10-29 14:33:19 +0530

    preorder traversal

---
+++ ds/src/Node.java
@@ -0,0 +1,11 @@
+/**
+ * Created by soham on 10/29/2017.
+ */
+class Node {
+    Node left, right;
+    int item;
+    Node(int item){
+        this.item = item;
+    }
+
+}
--- ds/src/TreeTraversal.java
+++ ds/src/TreeTraversal.java
@@ -1,9 +1,44 @@
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by soham on 10/29/2017.
  */
 public class TreeTraversal {
+    LinkedList<Node> stack = new LinkedList<>();
+
+    private void preOrderTraversal(Node root){
+        System.out.println(root);
+        stack.addFirst(root);
+
+        while (!stack.isEmpty()){
+            Node popedItem = stack.removeFirst();
+            System.out.println(popedItem.item);
+            if(popedItem.right!=null){
+                stack.addFirst(popedItem.right);
+            }
+            if(popedItem.left!=null){
+                stack.addFirst(popedItem.left);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Test");
+
+        Node root = new Node(2);
+        root.left = new Node(1);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(2);
+        root.right.left = new Node(1);
+
+        TreeTraversal tree = new TreeTraversal();
+        tree.preOrderTraversal(root);
+
+
     }
+
+
 }
 
